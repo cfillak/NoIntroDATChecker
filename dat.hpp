9848c66865e36a8ce698cc02b24997cb3c46592b
Christopher Fillak <cfilla2@gmail.com>
2017-04-11 19:50:06 -0500

    changing file extensions

---
+++ dat.hpp
@@ -0,0 +1,25 @@
+#ifndef DAT_H
+#define DAT_H
+
+#include <string>
+#include <iostream>
+#include <vector>
+#include "rom.h"
+
+class DAT
+{
+    public:
+        DAT();
+        
+
+
+    private:
+        vector<ROM> romList;
+
+
+};
+
+
+
+
+#endif
