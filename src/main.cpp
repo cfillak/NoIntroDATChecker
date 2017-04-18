989e306305e2bae4bac8189405ef912cbdb7cd3b
Christopher Fillak <cfilla2@gmail.com>
2017-04-18 17:39:14 -0500

    removed initial parse code

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "rom.hpp"
+#include "pugixml.hpp"
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -23,47 +24,5 @@ int main(int argc, char ** argv)
 
 int parse(string datFileName)
 {
-    string line;
-    cout << datFileName << '\n';
-    ifstream datfile(datFileName);
-
-    if (datfile.is_open())
-    {
-        while ( getline (datfile,line) )
-        {
-            if (line.compare(1,5,"rom (") == 0)
-            {
-                int namepos = line.find("name", 0);
-                int sizepos = line.find("size", namepos);
-                int crcpos = line.find("crc", sizepos);
-                int md5pos = line.find("md5", crcpos);
-                int sha1pos = line.find("sha1", md5pos);
-
-                string name = line.substr(namepos, sizepos-namepos);
-                name = name.substr(6);
-                name = name.substr(0, name.length()-2);
-                cout << name << '\n';
-
-                string size = line.substr(sizepos, crcpos-sizepos);
-                size = size.substr(5);
-                cout << size << '\n';
-
-                string crc = line.substr(crcpos, md5pos-crcpos);
-                crc = crc.substr(4);
-                cout << crc << '\n';
-
-                string md5 = line.substr(md5pos, sha1pos-md5pos);
-                md5 = md5.substr(4);
-                cout << md5 << '\n';
-
-                string sha1 = line.substr(sha1pos);
-                sha1 = sha1.substr(5);
-                cout << sha1 << '\n';
-
-            }
-
-        }
-    datfile.close();
-    }
-    return 0;
+    
 }
