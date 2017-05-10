74e4826ea3097cdaa08a0394da03a37f7333729a
Christopher Fillak <cfilla2@gmail.com>
2017-05-09 19:13:53 -0500

    lots of work finished

--- Makefile
+++ Makefile
@@ -1,17 +1,27 @@
 COMPILER = g++
 LINKER = g++
 
-datchecker : main.o rom.o dat.o
-	$(LINKER) main.o rom.o dat.o -o datchecker
 
-main.o : src/main.cpp src/rom.hpp
+datchecker : main.o pugixml.o hl_md5.o hl_md5wrapper.o hl_sha1.o hl_sha1wrapper.o
+	$(LINKER) main.o pugixml.o hl_md5.o hl_md5wrapper.o hl_sha1.o hl_sha1wrapper.o -o datchecker
+
+main.o : src/main.cpp src/pugixml.hpp src/hashlibpp.h
 	$(COMPILER) -c src/main.cpp
 
-rom.o : src/rom.cpp src/rom.hpp
-	$(COMPILER) -c src/rom.cpp
+pugixml.o : src/pugixml.cpp src/pugixml.hpp
+	$(COMPILER) -c src/pugixml.cpp
 
-dat.o : src/dat.cpp src/dat.hpp
-	$(COMPILER) -c src/dat.cpp
+hl_md5.o : src/hl_md5.cpp src/hl_md5.h
+	$(COMPILER) -c src/hl_md5.cpp
+
+hl_md5wrapper.o : src/hl_md5wrapper.cpp src/hl_md5wrapper.h
+	$(COMPILER) -c src/hl_md5wrapper.cpp
+
+hl_sha1.o : src/hl_sha1.cpp src/hl_sha1.h
+	$(COMPILER) -c src/hl_sha1.cpp
+
+hl_sha1wrapper.o : src/hl_sha1wrapper.cpp src/hl_sha1wrapper.h
+	$(COMPILER) -c src/hl_sha1wrapper.cpp
 
 clean :
 	@rm -f *.o
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,45 @@
-#include "rom.hpp"
+// #include "rom.hpp"
 #include "pugixml.hpp"
+
+#include "hl_exception.h"
+#include "hl_wrapperfactory.h"
+#include "hl_hashwrapper.h"
+#include "hl_md5.h"
+#include "hl_md5wrapper.h"
+#include "hl_sha1.h"
+#include "hl_sha1wrapper.h"
 #include <iostream>
 #include <fstream>
 #include <string>
 using namespace std;
+using namespace pugi;
+// using namespace MD5;
+std::string SetDir = "";
 
-int parse(string datFileName);
+int parse(const char * DatFileName);
+
+string StringToUpper(string strToConvert)
+{
+   for (string::iterator p = strToConvert.begin(); strToConvert.end() != p; ++p) {
+       *p = toupper(*p);
+   }
+   return strToConvert;
+}
+
+string md5Rom(const char * RomPath)
+{
+    md5wrapper::hashwrapper *wrap = new md5wrapper();
+    string md5(wrap->getHashFromFile(SetDir + "/" + RomPath));
+    return StringToUpper(md5);
+}
+
+string sha1Rom(const char * RomPath)
+{
+    sha1wrapper::hashwrapper *wrap = new sha1wrapper();
+    string sha1(wrap->getHashFromFile(SetDir + "/" + RomPath));
+    return StringToUpper(sha1);
+}
+
 
 int main(int argc, char ** argv)
 {
@@ -15,14 +49,35 @@ int main(int argc, char ** argv)
         cout << "Usage: ./datchecker [datfile.dat]" << '\n';
         return 0;
     }
+    SetDir = argv[2];
+    // cout << setDir << endl;
     parse(argv[1]);
-    // ROM *testRom = new ROM("mario.gb", 256, 1111, 2222, 3333);
-
+    // setDir = argv[2];
 
 }
 
 
-int parse(string datFileName)
+int parse(const char * DatFileName)
 {
-    
+    xml_document DatFile;
+    xml_parse_result result = DatFile.load_file(DatFileName, parse_default, encoding_auto);
+    xml_node root = DatFile.child("datafile");
+
+    for (xml_node MyNode = root.child("header"); MyNode; MyNode = MyNode.next_sibling("game"))
+    {
+        for (xml_attribute_iterator ait = MyNode.child("rom").attributes_begin(); ait != MyNode.child("rom").attributes_end(); ++ait)
+        {
+            if (strncmp(ait->name(), "name", 4) == 0) {
+                cout << " " << ait->name() << "=" << ait->value() << endl;
+                string md5 = md5Rom(ait->value());
+                string sha1 = sha1Rom(ait->value());
+                cout << "md5: " << md5 << endl;
+                cout << "sha1: " << sha1 << endl;
+            }
+
+        }
+
+    }
+
+    return 0;
 }
